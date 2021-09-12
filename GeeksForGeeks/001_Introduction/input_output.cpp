b78fa7caa7f8be859f4a4be010cb32e737274b06
vasanthkumar <42891954+vasanth9@users.noreply.github.com>
2021-09-12 15:25:40 +0530

    input and output from files

---
+++ GeeksForGeeks/001_Introduction/input_output.cpp
@@ -0,0 +1,16 @@
+// Below is C/C++ code for input/output
+#include<stdio.h>
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+	// For getting input from input.txt file
+	freopen("input.txt", "r", stdin);
+
+	// Printing the Output to output.txt file
+	freopen("output.txt", "w", stdout);
+
+#endif
+	return 0;
+}
---
+++ GeeksForGeeks/001_Introduction/input_through_cmd.cpp
@@ -0,0 +1,15 @@
+// A simple C++ code (test_code.cpp) which takes
+// one string as input and prints the same string
+// to output
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string S;
+	cin >> S;
+	cout << S;
+
+return 0;
+}
+//cmd /c 'a.exe < input.txt > output.txt ' 
