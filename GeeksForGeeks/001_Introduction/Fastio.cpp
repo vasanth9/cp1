4e4e75ac9b91dd899a0993c54e1c925c2db95fd3
vasanthkumar <42891954+vasanth9@users.noreply.github.com>
2021-09-12 14:59:50 +0530

    1

---
+++ GeeksForGeeks/001_Introduction/Fastio.cpp
@@ -0,0 +1,11 @@
+// It is often recommended to use scanf/printf instead of cin/cout for a fast input and output.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<"Hello World";
+    return 0;
+}
