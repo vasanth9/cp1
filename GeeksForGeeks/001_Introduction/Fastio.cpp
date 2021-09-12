329e7e9939b9e5fbf2d82b1fef2e2fc637dce715
vasanthkumar <42891954+vasanth9@users.noreply.github.com>
2021-09-12 15:25:22 +0530

    cpp

--- GeeksForGeeks/001_Introduction/Fastio.cpp
+++ GeeksForGeeks/001_Introduction/Fastio.cpp
@@ -1,11 +1,13 @@
 // It is often recommended to use scanf/printf instead of cin/cout for a fast input and output.
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> //include the standard template library (STL);
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout<<"Hello World";
+    cin.tie(NULL);                      //tie() is a method which simply guarantees the flushing of std::cout before std::cin accepts an input
+    cout<<"Hello World\n";             //use \n instead of endl;
     return 0;
 }
+
+//https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
